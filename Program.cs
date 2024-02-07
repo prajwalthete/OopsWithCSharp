ef1ca353c41a591eeec26b8c312ac02ebe3960e8
prajwal <prajwalthete98@gmail.com>
2024-02-07 12:09:40 +0530

    "[PRAJWAL] Add : Abstraction Implementation With Class "

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Oops
+﻿namespace abstraction
 {
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+    class Circle : Shape
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO OOPS WITH C-SHARP");
+            Circle c = new Circle(10);
+            Console.WriteLine(c.CalculateArea());
         }
     }
 
