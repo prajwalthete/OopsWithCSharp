fa656769edc595458533cdf7efe0b0f8f361be97
prajwal <prajwalthete98@gmail.com>
2024-02-07 11:28:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace inheritance
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("animal eating");
+        }
+    }
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("woof,woof");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+
+
+
+
+
+}
+
