6f2e7739b42cb726dd5e70d3a5c72593ba8ce46c
prajwal <prajwalthete98@gmail.com>
2024-02-07 12:18:35 +0530

    "[PRAJWAL] Add : Abstraction With Interface Implementation"

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Oops
+﻿namespace abstrction_With_Interface
 {
+
+    public interface Ishape
+    {
+        double CalculateArea();
+    }
+
+    class Circle : Ishape
+    {
+        private double Radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO OOPS WITH C-SHARP");
+            Circle c = new Circle(10);
+            Console.WriteLine(c.CalculateArea());
         }
     }
 
